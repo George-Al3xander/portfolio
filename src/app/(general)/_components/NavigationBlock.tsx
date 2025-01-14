82e758be7b2259460584050d4d076953d49f8b15
George <george.valuiskyi@gmail.com>
2025-01-14 21:13:07 +0200

    feat: navigation block
    Add a NavigationBlock component that includes both desktop and mobile
    navigation logic, and create a constant file for the website's routes.

---
+++ src/app/(general)/_components/NavigationBlock.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/ui/components/atoms/Button";
+import { NavBar } from "@/ui/components/molecules/NavBar";
+import { ResponsiveDrawer } from "@/ui/components/organisms/ResponsiveDrawer";
+import { MenuIcon } from "lucide-react";
+import { WEBSITE_ROUTES } from "../constants/routes";
+import { Logo } from "./Logo";
+
+const MENU_DESCRIPTION =
+    "Navigate through my portfolio to explore my projects, skills, experience, and get in touch with me for collaboration or inquiries.";
+
+const TRIGGER_SR_TEXT = "Open navigation menu";
+
+export const NavigationBlock = () => (
+    <ResponsiveDrawer
+        triggerWrapperProps={{
+            asChild: true,
+            children: (
+                <Button
+                    type={"button"}
+                    variant="outline"
+                    size="icon"
+                    className={"rounded-full bg-accent"}
+                >
+                    <span className={"sr-only"}>{TRIGGER_SR_TEXT}</span>
+                    <MenuIcon />
+                </Button>
+            ),
+        }}
+        title={<Logo mainComponentTag={"p"} hideTextOnMobile={false} />}
+        description={MENU_DESCRIPTION}
+    >
+        <NavBar links={WEBSITE_ROUTES} />
+    </ResponsiveDrawer>
+);
---
+++ src/app/(general)/constants/routes.ts
@@ -0,0 +1,18 @@
+export const WEBSITE_ROUTES: { title: string; href: string }[] = [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "Projects",
+        href: "/projects",
+    },
+    {
+        title: "About",
+        href: "/about",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+    },
+];
