9cafe4bdb3bfa860c394683304714fe186ecd3b5
George <george.valuiskyi@gmail.com>
2025-01-15 09:51:06 +0200

    feat: icon link
    Add IconLink component.

---
+++ src/ui/components/atoms/IconLink/IconLink.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/utils/cn";
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
+import { ComponentProps, FC, HTMLAttributes } from "react";
+
+type Props = Omit<ComponentProps<typeof Link>, "children"> & {
+    icon: LucideIcon;
+    className?: HTMLAttributes<HTMLLinkElement>["className"];
+    srText: string;
+};
+
+export const IconLink: FC<Props> = ({
+    icon: Icon,
+    srText,
+    className,
+    ...props
+}) => (
+    <Link
+        {...props}
+        className={cn(
+            className,
+            "text-foreground transition-all hover:text-primary hover:opacity-60",
+        )}
+    >
+        <Icon />
+        <span className="sr-only">{srText}</span>
+    </Link>
+);
