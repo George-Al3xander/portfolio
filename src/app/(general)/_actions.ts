e098b0dea7403ea60af8bc29ed63127949b1d1d9
George <george.valuiskyi@gmail.com>
2025-01-20 12:48:12 +0200

    feat: get information by id action
    Create a server action to retrieve a document from the 'information'
    collection in Firestore by its ID.

--- src/app/(general)/_actions.ts
+++ src/app/(general)/_actions.ts
@@ -1,3 +1,12 @@
 import * as internalApi from "@/services/api/internal";
+import { TInformation } from "@/types/models/Information";
 
 export const getExperience = async () => await internalApi.experience.findAll();
+
+export const getInformationById = async (id: string): Promise<TInformation> => {
+    const item = await internalApi.information.findById(id);
+
+    if (!item) throw new Error(`Could not find information with id '${id}'`);
+
+    return item;
+};
