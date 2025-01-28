dafeff0e73fe6b51236e5174ec2a0a1fb72ea68a
George <george.valuiskyi@gmail.com>
2025-01-28 12:13:24 +0200

    test: mock env variables
    Add mocks of the environment variables and update the Jest setup file.

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom";
+import { mockEnv } from "./src/mocks/env.mock";
+
+jest.mock("@t3-oss/env-nextjs", () => ({
+    createEnv: jest.fn(() => mockEnv),
+}));
---
+++ src/mocks/env.mock.ts
@@ -0,0 +1,13 @@
+export const mockEnv = {
+    NODE_ENV: "production",
+    API_KEY: "your_api_key_here",
+    AUTH_DOMAIN: "your_auth_domain_here",
+    PROJECT_ID: "your_project_id_here",
+    STORAGE_BUCKET: "your_storage_bucket_here",
+    SENDER_ID: "your_sender_id_here",
+    APP_ID: "your_app_id_here",
+    EMAILJS_SERVICE_ID: "your_emailjs_service_id_here",
+    EMAILJS_TEMPLATE_ID: "your_emailjs_template_id_here",
+    EMAILJS_PUBLIC_KEY: "your_emailjs_public_key_here",
+    EMAILJS_PRIVATE_KEY: "your_emailjs_private_key_here",
+} as const;
